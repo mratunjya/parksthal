aae0a1ee7a04ebbf8294a0ab65daa8967080e311
Mratunjya Shankhdhar <mratunjya13@gmail.com>
2023-11-24 14:32:59 +0530

    Added Private Layout Functionality to Older Layout with no flashes

--- src/layout.js
+++ src/layout.js
@@ -1,12 +1,26 @@
 import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import isPropValid from "@emotion/is-prop-valid";
 import ProgressBar from "react-scroll-progress-bar";
 import { StyleSheetManager } from "styled-components";
 
 import NavBar from "@common/NavBar";
+import { Loader } from "@common/Loader";
 import { SECONDARY_500 } from "@colors";
+import { IsAuthenticated, SessionStatus } from "@Auth";
 
-export default function Layout({ title, description, children }) {
+export default function Layout({ title, description, children, privateRoute }) {
+  const router = useRouter();
+  const status = SessionStatus();
+  const isAuthenticated = IsAuthenticated();
+
+  useEffect(() => {
+    if (privateRoute) {
+      status !== "loading" && !isAuthenticated && router.push("log-in");
+    }
+  }, [isAuthenticated, privateRoute, router, status]);
+
   return (
     <>
       <Head>
@@ -24,7 +38,15 @@ export default function Layout({ title, description, children }) {
       <ProgressBar bgcolor={SECONDARY_500} height="3px" />
       <StyleSheetManager shouldForwardProp={(prop) => isPropValid(prop)}>
         <NavBar />
-        <main>{children}</main>
+        {privateRoute ? (
+          status === "loading" ? (
+            <Loader flex="1" />
+          ) : (
+            isAuthenticated && <main>{children}</main>
+          )
+        ) : (
+          <main>{children}</main>
+        )}
       </StyleSheetManager>
     </>
   );
